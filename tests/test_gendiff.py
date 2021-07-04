036021990c019651a04e164472699985be4a55aa
Angekok <angekok@mail.ru>
2021-07-04 19:19:21 +0300

    Add test files.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,4 +7,4 @@ from gendiff.gendiff import generate_diff
 test_flat_json = ''.join(open('tests/gendiff.txt', 'r'))
 
 def test_full_load():
-    assert generate_diff('file1.json', 'file2.json') == str(test_flat_json)
+    assert generate_diff('tests/file1.json', 'tests/file2.json') == str(test_flat_json)
