9682991607fc05996750a8572106d59402fbcfd5
Angekok <angekok@mail.ru>
2021-07-04 19:43:52 +0300

    Fixed the composition of the testing package.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,7 +4,7 @@
 
 from gendiff.gendiff import generate_diff
 
-test_flat_json = ''.join(open('tests/gendiff.txt', 'r'))
+test_flat_json = ''.join(open('tests/fixtures/json_out.txt', 'r'))
 
 def test_full_load():
-    assert generate_diff('tests/file1.json', 'tests/file2.json') == str(test_flat_json)
+    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str(test_flat_json)
