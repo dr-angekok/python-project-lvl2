2caacbd8b66d6e576ed748051cdf54af708d2bf3
Angekok <angekok@mail.ru>
2021-07-05 19:14:34 +0300

    Add test fore yaml input output.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,7 +4,11 @@
 
 from gendiff.gendiff import generate_diff
 
-test_flat_json = ''.join(open('tests/fixtures/json_out.txt', 'r'))
+simple_out = ''.join(open('tests/fixtures/json_out.txt', 'r'))
 
-def test_full_load():
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str(test_flat_json)
+def test_full_load_json():
+    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str(simple_out)
+
+
+def test_full_load_yaml():
+    assert generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml') == str(simple_out)
