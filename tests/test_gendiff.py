adf23fc385bfb6be7ff2d0ee61dff49459707719
Angekok <angekok@mail.ru>
2021-07-07 21:14:26 +0300

    Updating tests for new tasks.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,19 +5,14 @@ from os import popen
 from gendiff.gendiff import generate_diff
 
 simple_out = ''.join(open('tests/fixtures/json_out.txt', 'r'))
-
-def test_full_load_json():
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == str(simple_out)
+complex_out = ''.join(open('tests/fixtures/grand_out.txt', 'r'))
 
 
-def test_full_load_yaml():
-    assert generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml') == str(simple_out)
-
-
-def test_full_load_yml():
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml') == str(simple_out)
-
-
-def test_gendiff_cli():
+def test_gendiff_cli_flat():
     result = popen('poetry run gendiff tests/fixtures/file1.yaml tests/fixtures/file2.yaml').read()
     assert str(result[:-1]) == str(simple_out)
+
+
+def test_grandiff_cli_flat():
+    result = popen('poetry run gendiff tests/fixtures/grand_file1.yaml tests/fixtures/grand_file2.yaml').read()
+    assert str(result[:-1]) == str(complex_out)
