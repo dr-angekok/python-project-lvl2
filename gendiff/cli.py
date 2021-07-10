04a964360bb80d43813ff05b9629080e37b5f1da
Angekok <angekok@mail.ru>
2021-07-10 16:11:32 +0300

    Expand help string.

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -2,6 +2,10 @@
 """Cli for the diff make."""
 import argparse
 
+FORMAT_HELP_STRING = """
+set format of output
+stylish -default"""
+
 
 def arg_parser():
     """Pars command line parameters.
@@ -9,9 +13,9 @@ def arg_parser():
     Returns:
         tuple: str of first_file, second_file, format
     """
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', metavar='file1.json', type=str)
-    parser.add_argument('second_file', metavar='file2.json', type=str)
-    parser.add_argument('-f', '--format', help='\n set format of output')
+    parser = argparse.ArgumentParser(description='Generate diff of two files')
+    parser.add_argument('first_file', metavar='file1.[json/yaml]', type=str)
+    parser.add_argument('second_file', metavar='file2.[json/yaml]', type=str)
+    parser.add_argument('-f', '--format', help=FORMAT_HELP_STRING)
     arguments = parser.parse_args()
     return arguments.first_file, arguments.second_file, arguments.format
