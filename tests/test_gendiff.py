98d2ad9de6a8fd4488c176f7d53658b79bb03e75
Angekok <angekok@mail.ru>
2021-07-12 21:02:11 +0300

    Add test fore plain output.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,10 +2,10 @@
 """Users tests."""
 from os import popen
 
-from gendiff.gendiff import generate_diff
 
 simple_out = ''.join(open('tests/fixtures/json_out.txt', 'r'))
 complex_out = ''.join(open('tests/fixtures/grand_out.txt', 'r'))
+plain_out = ''.join(open('tests/fixtures/plain_out.txt', 'r'))
 
 
 def test_gendiff_cli_flat():
@@ -16,3 +16,8 @@ def test_gendiff_cli_flat():
 def test_grandiff_cli_flat():
     result = popen('poetry run gendiff tests/fixtures/grand_file1.yaml tests/fixtures/grand_file2.yaml').read()
     assert str(result) == str(complex_out)
+
+
+def test_grandiff_cli_plain():
+    result = popen('poetry run gendiff tests/fixtures/grand_file1.json tests/fixtures/grand_file2.json -f plain').read()
+    assert str(result) == str(plain_out)
