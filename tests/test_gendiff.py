16fa90e9402bfb5473fdebe80d1d33ea93a8f2c8
Angekok <angekok@mail.ru>
2021-07-26 22:20:10 +0300

    Tests have been redone.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,29 +1,45 @@
 # -*- coding:utf-8 -*-
 """Users tests."""
+import json
 from os import popen
 
-
-simple_out = ''.join(open('tests/fixtures/json_out.txt', 'r'))
-complex_out = ''.join(open('tests/fixtures/grand_out.txt', 'r'))
-plain_out = ''.join(open('tests/fixtures/plain_out.txt', 'r'))
-json_out = ''.join(open('tests/fixtures/json_format_out.txt', 'r'))
+import pytest
 
 
-def test_gendiff_cli_flat():
-    result = popen('poetry run gendiff tests/fixtures/file1.yaml tests/fixtures/file2.yaml').read()
-    assert str(result) == str(simple_out)
+def simple_out():
+    with open('tests/fixtures/json_out.txt', 'r') as file:
+        out = file.read()
+    return str(out)
 
 
-def test_grandiff_cli_flat():
-    result = popen('poetry run gendiff tests/fixtures/grand_file1.yaml tests/fixtures/grand_file2.yaml').read()
-    assert str(result) == str(complex_out)
+def complex_out():
+    with open('tests/fixtures/grand_out.txt', 'r') as file:
+        out = file.read()
+    return str(out)
 
 
-def test_grandiff_cli_plain():
-    result = popen('poetry run gendiff tests/fixtures/grand_file1.json tests/fixtures/grand_file2.json -f plain').read()
-    assert str(result) == str(plain_out)
+def plain_out():
+    with open('tests/fixtures/plain_out.txt', 'r') as file:
+        out = file.read()
+    return str(out)
+
+
+def json_out():
+    with open('tests/fixtures/json_format_out.txt', 'r') as file:
+        out = file.read()
+    return str(out)
+
+
+@pytest.mark.parametrize("file1,file2,param,expected", [
+    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '', simple_out()),
+    ('tests/fixtures/grand_file1.yaml', 'tests/fixtures/grand_file2.yaml', '', complex_out()),
+    ('tests/fixtures/grand_file1.json', 'tests/fixtures/grand_file2.json', '-f plain', plain_out())
+    ])
+def test_with_cli(file1, file2, param, expected):
+    result = str(popen('poetry run gendiff {0} {1} {2}'.format(file1, file2, param)).read())
+    assert result == expected
 
 
 def test_json_out():
-    result = popen('poetry run gendiff tests/fixtures/grand_file1.json tests/fixtures/grand_file2.json -f json').read()
-    assert str(result) == str(json_out)
+    result = str(popen('poetry run gendiff tests/fixtures/grand_file1.json tests/fixtures/grand_file2.json -f json').read())
+    assert json.loads(result) == json.loads(json_out())
