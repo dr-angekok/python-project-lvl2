cf2e92c32df5307129fef38644c45c293b6f4106
Angekok <angekok@mail.ru>
2021-07-26 22:38:42 +0300

    Fix lint issues.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -33,8 +33,7 @@ def json_out():
 @pytest.mark.parametrize("file1,file2,param,expected", [
     ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', '', simple_out()),
     ('tests/fixtures/grand_file1.yaml', 'tests/fixtures/grand_file2.yaml', '', complex_out()),
-    ('tests/fixtures/grand_file1.json', 'tests/fixtures/grand_file2.json', '-f plain', plain_out())
-    ])
+    ('tests/fixtures/grand_file1.json', 'tests/fixtures/grand_file2.json', '-f plain', plain_out())])
 def test_with_cli(file1, file2, param, expected):
     result = str(popen('poetry run gendiff {0} {1} {2}'.format(file1, file2, param)).read())
     assert result == expected
